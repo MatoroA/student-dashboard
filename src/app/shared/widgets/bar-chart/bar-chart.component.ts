5e0a653899631416ee0dd193ac0f693d1a3663ab
andani matoro <MatoroA>
2020-01-31 11:55:20 +0200

    friday

--- src/app/shared/widgets/bar-chart/bar-chart.component.ts
+++ src/app/shared/widgets/bar-chart/bar-chart.component.ts
@@ -4,119 +4,110 @@ import HC_exporting from 'highcharts/modules/exporting';
 import { ApiService } from 'src/app/backend/api.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Course } from 'src/app/models/course';
+import { RegisteredUser } from 'src/app/models/registered-user';
 
 @Component({
-  selector: 'app-bar-chart',
-  templateUrl: './bar-chart.component.html',
-  styleUrls: ['./bar-chart.component.scss']
+    selector: 'app-bar-chart',
+    templateUrl: './bar-chart.component.html',
+    styleUrls: ['./bar-chart.component.scss']
 })
 export class BarChartComponent implements OnInit {
+    Highcharts: typeof Highcharts = Highcharts;
 
-  chartOptions : {};
-  series:any;
-  coursename : Course ;
-  array1: any[];
-  details:{name:string,y:number}
+    chartOptions = {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Registered students'
+        },
 
-  Highcharts = Highcharts;
-    
-  constructor(private apiservice : ApiService ,private afs: AngularFirestore,) {
+        accessibility: {
+            announceNewData: {
+                enabled: true
+            }
+        },
+        xAxis: {
+            type: 'category'
+        },
+        yAxis: {
+            title: {
+                text: 'Total percent market share'
+            }
 
-    afs.collection("courses").snapshotChanges().subscribe((data:any) =>{ 
-       
-        this.coursename = data.map ( e => {
-            return{
-              key: e.payload.doc.id,
-              ...e.payload.doc.data()
-            } as Course
-          });
-    } );
-    
-    
-   }
+        },
+        legend: {
+            enabled: false
+        },
+        // plotOptions: {
+        //     series: {
+        //         borderWidth: 0,
+        //         dataLabels: {
+        //             enabled: true,
+        //             format: '{point.y:.1f}%'
+        //         }
+        //     }
+        // },
 
-  ngOnInit() {
+        tooltip: {
+            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+        },
 
-    this.chartOptions= {
-      chart: {
-          type: 'column'
-      },
-      title: {
-          text: 'Registered students'
-      },
-      
-      accessibility: {
-          announceNewData: {
-              enabled: true
-          }
-      },
-      xAxis: {
-          type: 'category'
-      },
-      yAxis: {
-          title: {
-              text: 'Total percent market share'
-          }
-  
-      },
-      legend: {
-          enabled: false
-      },
-      plotOptions: {
-          series: {
-              borderWidth: 0,
-              dataLabels: {
-                  enabled: true,
-                  format: '{point.y:.1f}%'
-              }
-          }
-      },
-  
-      tooltip: {
-          headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-          pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-      },
-  
-      series: [
-          {
-              name: "courses",
-              colorByPoint: true,
-              data:  [
-               
-                  {
-                      name: "brick laying",
-                      y: 62.74,
-                      
-                  },
-                  {
-                      name: "plumbing",
-                      y: 10.57,
-                 
-                  },
-                  {
-                      name: " Electrical Enginering",
-                      y: 7.23,
-                      
-                  },
-                  {
-                      name: "capentry",
-                      y: 5.58,
-                      
-                  }
+        series: [
+            {
+                name: "courses",
+                colorByPoint: true,
+                data: []
+            }
+        ],
 
-              ]
-          }
-      ],
-    
-  };
+    };
+    series: any;
+    coursename: Course;
+    array1: any[];
+    details: { name: string, count: number }
 
-  HC_exporting(Highcharts);
-  setTimeout(()=>{
-    window.dispatchEvent(
-      new Event('resize')
-    );
-  }, 300)
-}
+    private registeredStudents: RegisteredUser[];
 
+    constructor(private _apiService: ApiService, private afs: AngularFirestore) {
+
+
+
+
+    }
+
+     ngOnInit() {
+     this.getData()
+        // console.log(this.registeredStudents);
+
+
+        // this.graphData();
+    }
+
+     getData() {
+
+        console.log("one")
+         this._apiService.getCourses().subscribe(courseList => {
+            this.registeredStudents = [];
+            console.log(courseList)
+            for (let course of courseList) {
+                let item = new RegisteredUser();
+                item.setCourseName(course.name);
+                // item.setId(course.id);
+                this.registeredStudents.push(item);
+
+                this._apiService.getStudent(course.id).subscribe(studentApplications => {
+                    for (let application of studentApplications) {
+                        if (application.status) {
+                            item.addStudentsCount();
+                        }
+                    }
+                });
+            }     
+        });
+        this.chartOptions.series[0].data = [{name:'', y: 5}];
+        console.log(this.chartOptions.series[0].data)
+    }
 
 }
