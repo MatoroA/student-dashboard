31d9209a45b1561ca2a0322dc33c23a900a820dd
andani matoro <MatoroA>
2020-02-13 11:21:52 +0200

    ......

--- src/app/can-activate-route.guard.ts
+++ src/app/can-activate-route.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './backend/auth.service';
 
@@ -8,10 +8,13 @@ import { AuthService } from './backend/auth.service';
 })
 export class CanActivateRouteGuard implements CanActivate {
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-      return this.auth.isUserAuthenticated();
+    if (!this.auth.isUserAuthenticated()) {
+      this.router.navigate(["/"])
+    }
+    return this.auth.isUserAuthenticated();
   }
-  
+
 }
